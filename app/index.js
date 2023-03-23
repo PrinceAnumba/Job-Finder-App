7a7f9a72b26b5d04e5ba8582b8e9ff951200b7fd
Prince <princeanumba0@gmail.com>
2023-03-23 11:08:41 +0000

    structure set

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hi, I am Prince</Text>
+        </View>
+    )
+}
+
+export default Home;
